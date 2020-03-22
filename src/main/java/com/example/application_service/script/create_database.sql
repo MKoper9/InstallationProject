f52f6259c4506830259f3ee7ffbfa14ad73d3a60
Michal Koperski <mkoperpno@gmali.com>
2020-03-22 20:57:17 +0100

    add new sql file

---
+++ src/main/java/com/example/application_service/script/create_database.sql
@@ -0,0 +1,15 @@
+-- create database
+create database calculator_service_db
+    character set utf8 collate utf8_polish_ci;
+
+set @@global.time_zone = '+2:00';
+
+-- create new user
+-- create, drop, alter, insert, update, select, delete
+create user 'customer_service_user'@'localhost' identified by 'Qwerty123';
+grant
+    create, drop, alter, insert, update, select, delete, references
+    on
+    calculator_service_db.*
+    to
+    'calculator_service_user'@'localhost';
